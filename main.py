141007531d1cc881c6cc3b3236cbb31c4f448928
Ken Valverde <verify>
2020-06-14 16:40:03 -0500

    added user input functionality

--- main.py
+++ main.py
@@ -13,8 +13,17 @@ months = {
     'december': '12'
 }
 
+days = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16',
+        '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29' '30', '31']
+
 input_file = open('inputDates.txt', 'r')
 output_file = open('parsedDates.txt', 'w')
+
+# Extracts dates from input_file and outputs formatted date into new parsedDates.txt file
+print('opening inputDates.txt file...')
+print('parsing...')
+print('outputting formatted date in parsedDates.txt file...')
+print('\n' + 'OUTPUT:')
 
 for entry in input_file:
     if entry != '-1': # as long as inputs are not -1
@@ -38,5 +47,37 @@ for entry in input_file:
 
 output_file.close()
 input_file.close()
+
+# ---------------------------------------------------------------
+# Converts user input of whatever date, parses it, and outputs formatted version.
+print()
+print('-' * 45)
+print('TRY IT YOURSELF!')
+print('Please use the following format: September 1, 2020')
+
+user_date = input('What is the date: ')
+
+for user_entry in user_date:
+    if user_entry != '-1':
+        tokens = user_date.split()
+        if len(tokens) >= 3:
+            month = tokens[0]
+            day = tokens [1]
+            year = tokens [2]
+            month = month.lower()
+
+            if month in months and day[:len(day)-1] in days:
+                day_with_comma = day[-1]
+                if day_with_comma == ',':
+                    day = day[:len(day)-1]
+                    month_number = months[month]
+                    print('The date is:', month_number + '/' + day + '/' + year)
+                    break
+            else:
+                print('ERROR: Please follow the format shown above.')
+                break
+        else:
+            print('ERROR: Please follow the format shown above.')
+            break
 
 
